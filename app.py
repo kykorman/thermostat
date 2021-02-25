83fa9212aa6293f203a53a8daac3fa87d708b699
Kyle Korman <36543014+kykorman@users.noreply.github.com>
2021-02-24 22:01:40 -0600

    Update app.py
    Significant updates, switched to a dictionary to contain pins and everything. Trying to implement socketIO things for bi-directional data transfer.

--- app.py
+++ app.py
@@ -2,101 +2,116 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from flask import redirect, url_for
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 import threading
 import Adafruit_DHT as dht
 import time
 import gpiozero as gpio
-
+import json
+import eventlet
 
 h=""
 t=""
-hvacFan="auto"
-hvacMode="cool"
-hvacHeat=68.0
-hvacCool=80.0
 
-heatPin=""
-coolPin=""
-fanPin=""
-dhtPin=4
-hvacState = False
-socketio=""
+controlDict = {}
 
+app = Flask(__name__)
+
+@app.before_first_request
 def create_application():
-    global hvacState, heatPin, coolPin, fanPin, dhtPin, hvacFan, hvacMode, hvacHeat, hvacCool, h, t
-    global app
-    global socketio
-    heatPin=gpio.LED(26)
-    coolPin=gpio.LED(20)
-    fanPin=gpio.LED(21)
-    dhtPin=4
+    global controlDict, h, t
+
+    controlDict['heatPin']=gpio.LED(26)
+    controlDict['coolPin']=gpio.LED(20)
+    controlDict['fanPin']=gpio.LED(21)
+
+    controlDict['heatPin'].off()
+    controlDict['coolPin'].off()
+    controlDict['fanPin'].off()
+    controlDict['hvacState'] = False
 
 
+    #TODO replace with reading/writing as JSON
     with open('/root/thermo/settings.txt') as f:
-        hvacFan = f.readline()
-        hvacMode = f.readline()
-        hvacHeat = float(f.readline())
-        hvacCool = float(f.readline())
+        controlDict['hvacFan'] = f.readline().rstrip()
+        controlDict['hvacMode'] = f.readline().rstrip()
+        controlDict['hvacHeat'] = float(f.readline())
+        controlDict['hvacCool'] = float(f.readline())
 
+    if controlDict['hvacFan'] == "on":
+        controlDict['fanPin'].on()
        
     def runHvac():
-        global  hvacState, heatPin, coolPin, fanPin, dhtPin, hvacFan, hvacMode, hvacHeat, hvacCool, h, t
+        global  controlDict, h, t
 
-        heatPin.off()
-        coolPin.off()
-        time.sleep(20) # sleep for 10 seconds, check the sensor first
+        controlDict['heatPin'].off()
+        controlDict['coolPin'].off()
+        controlDict['hvacState'] = False
+        
         while True:
-            if(hvacMode=="heat" and t < hvacHeat -0.75):
+            ## print("HvacMode: {} fanMode: {} hvacState: {} hvacCoolTemp: {} hvacHeatTemp: {}".format(controlDict['hvacMode'], controlDict['hvacFan'], controlDict['hvacState'], controlDict['hvacCool'], controlDict['hvacHeat']))
+            if(controlDict['hvacMode']=="heat" and t < controlDict['hvacHeat'] -0.75):
                 #check if temp is lower
-                if(hvacFan != "on"):
-                    fanPin.on()
-                hvacState = True
+                if(controlDict['hvacFan'] != "on"):
+                    controlDict['fanPin'].on()
+                controlDict['hvacState'] = True
                 time.sleep(5)
 
-                heatPin.on()
+                controlDict['heatPin'].on()
                 
-                while(t < hvacHeat +0.75):
+                while(t < controlDict['hvacHeat'] +0.75 and controlDict['hvacMode'] == "heat"):
                     time.sleep(10)
 
-                heatPin.off()
+                controlDict['heatPin'].off()
                 time.sleep(5)
-                if(hvacFan != "on"):
-                    fanPin.off()
-                hvacState = False
+                controlDict['hvacState'] = False
+                if(controlDict['hvacFan'] != "on"):
+                    controlDict['fanPin'].off()
 
-            elif(hvacMode=="cool" and t > hvacCool + 0.75):
+
+            elif(controlDict['hvacMode']=="cool" and t > controlDict['hvacCool'] + 0.75):
                 #check if temp is lower
-                if(hvacFan != "on"):
-                    fanPin.on()
-                hvacState = True
+                if(controlDict['hvacFan'] != "on"):
+                    controlDict['fanPin'].on()
+                controlDict['hvacState'] = True
                 time.sleep(5)
 
-                coolPin.on()
+                controlDict['coolPin'].on()
                 
-                while(t > hvacCool -0.75):
+                while(t > controlDict['hvacCool'] -0.75 and controlDict['hvacMode'] == "cool"):
                     time.sleep(10)
 
-                coolPin.off()
+                controlDict['coolPin'].off()
                 time.sleep(5)
-                if(hvacFan != "on"):
-                     fanPin.off()
+                controlDict['hvacState'] = False
+                if(controlDict['hvacFan'] != "on"):
+                     controlDict['fanPin'].off()
+            
+            time.sleep(10)
 
-                hvacState = False
 
     def checkSensors():
-        global hvacState, heatPin, coolPin, fanPin, dhtPin, hvacFan, hvacMode, hvacHeat, hvacCool, h, t
+        global controlDict, h, t
+        dhtPin = 4
         while True:
             h,temp = dht.read_retry(dht.DHT22, dhtPin)
-            #Print Temperature and Humidity on Shell window
-            t = temp * (9/5) + 32
-            print('Temp={0:0.1f}*F  Humidity={1:0.1f}%'.format(t,h))
-            time.sleep(10) # only check sensor every 10 seconds
+            
+            #Ensure kind of reasonable temp sensor values; if it errored, it would probably say -127 or 127
+            temp = temp * (9/5) + 32
+            if(temp>=40 and temp<=100):
+                t = temp
+            
+            ## Debug sensor
+            print('Temp={0:0.1f}*F  Humidity={1:0.1f}%'.format(temp,h))
+            time.sleep(10) # only check sensor every 10 seconds    
+        
+    
     hvacThread = threading.Thread(target=runHvac)
     sensThread = threading.Thread(target=checkSensors)
 
 
     sensThread.start()
+    time.sleep(5)
     hvacThread.start()
 
 def withinTempRange(temp):
@@ -104,69 +119,82 @@ def withinTempRange(temp):
         return True
     return False
 
-create_application()
-app = Flask(__name__)
-
-socketio = SocketIO(app)
-print("Here")
-socketio.run(app)
-
 @app.route('/')
 def home():
-
     return render_template('home.html')
 
-@app.route('/heat')
-def heat():
-    global hvacState, heatPin, coolPin, fanPin, dhtPin, hvacFan, hvacMode, hvacHeat, hvacCool, h, t
-    hvacMode="heat"
-    coolPin.off()
+# Go to /temp/heat or /temp/cool to set temp without changing operating mode. Could be useful for things like smart home integration.
+@app.route('/temp/<hvacHC>')
+def tempSet(hvacHC):
+    global controlDict
 
     temp = request.args.get('temp', default = None, type = float)
     if(temp is not None and withinTempRange(temp)):
-        hvacHeat = temp
+        if(hvacHC == "heat"):
+            controlDict['hvacHeat']=temp
+        elif(hvacHC == "cool"):
+            controlDict['hvacCool']=temp
 
     with open('/root/thermo/settings.txt', 'w') as f:
-        f.write(hvacFan + "\n")
-        f.write(hvacMode + "\n")
-        f.write("{:.2f}\n".format(hvacHeat))
-        f.write("{:.2f}\n".format(hvacCool))
-        
+        f.write(controlDict['hvacFan'] + "\n")
+        f.write(controlDict['hvacMode'] + "\n")
+        f.write("{:.2f}\n".format(controlDict['hvacHeat']))
+        f.write("{:.2f}\n".format(controlDict['hvacCool']))
+
+    return redirect(url_for('home'))
+
+@app.route('/heat')
+def heat():
+    global controlDict
+    controlDict['hvacMode']="heat"
+    controlDict['coolPin'].off()
+
+    temp = request.args.get('temp', default = None, type = float)
+    if(temp is not None and withinTempRange(temp)):
+        controlDict['hvacHeat'] = temp
+
+    with open('/root/thermo/settings.txt', 'w') as f:
+        f.write(controlDict['hvacFan'] + "\n")
+        f.write(controlDict['hvacMode'] + "\n")
+        f.write("{:.2f}\n".format(controlDict['hvacHeat']))
+        f.write("{:.2f}\n".format(controlDict['hvacCool']))
+
     return redirect(url_for('home'))
 
 @app.route('/cool')
 def cool():
-    global hvacState, heatPin, coolPin, fanPin, dhtPin, hvacFan, hvacMode, hvacHeat, hvacCool, h, t
-    hvacMode="cool"
-    heatPin.off()
+    global controlDict
+    controlDict['hvacMode']="cool"
+    controlDict['heatPin'].off()
 
     temp = request.args.get('temp', default = None, type = float)
     if(temp is not None and withinTempRange(temp)):
-        hvacCool = temp
+        controlDict['hvacCool'] = temp
 
     with open('/root/thermo/settings.txt','w') as f:
-        f.write(hvacFan + "\n")
-        f.write(hvacMode + "\n")
-        f.write("{:.2f}\n".format(hvacHeat))
-        f.write("{:.2f}\n".format(hvacCool))
+        f.write(controlDict['hvacFan'] + "\n")
+        f.write(controlDict['hvacMode'] + "\n")
+        f.write("{:.2f}\n".format(controlDict['hvacHeat']))
+        f.write("{:.2f}\n".format(controlDict['hvacCool']))
+
     return redirect(url_for('home'))
 
 @app.route('/fan')
 def fan():
-    global hvacState, heatPin, coolPin, fanPin, dhtPin, hvacFan, hvacMode, hvacHeat, hvacCool, h, t
+    global controlDict
 
-    if(hvacFan == "auto"):
-        hvacFan = "on"
-        fanPin.on()
+    if(controlDict['hvacFan'] == "auto"):
+        controlDict['hvacFan'] = "on"
+        controlDict['fanPin'].on()
     else:
-        hvacFan = "auto"
-        if(not hvacState): 
-            fanPin.off()
+        controlDict['hvacFan'] = "auto"
+        if(not controlDict['hvacState']): 
+            controlDict['fanPin'].off()
 
     with open('/root/thermo/settings.txt','w') as f:
-        f.write(hvacFan + "\n")
-        f.write(hvacMode + "\n")
-        f.write("{:.2f}\n".format(hvacHeat))
-        f.write("{:.2f}\n".format(hvacCool))
-    return redirect(url_for('home'))
+        f.write(controlDict['hvacFan'] + "\n")
+        f.write(controlDict['hvacMode'] + "\n")
+        f.write("{:.2f}\n".format(controlDict['hvacHeat']))
+        f.write("{:.2f}\n".format(controlDict['hvacCool']))
 
+    return redirect(url_for('home'))
